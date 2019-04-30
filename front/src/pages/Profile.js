e74615d7d1c02898ea76439a59458ab4723df3b8
Mohamad Alkel <arcmohammadalkl@gmail.com>
2019-04-30 13:44:57 +0800

    add the card for project

--- front/src/pages/Profile.js
+++ front/src/pages/Profile.js
@@ -1,43 +1,106 @@
 import React, { Component } from 'react';
-import '../styles/profile.css';
 import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
   CardSubtitle, CardBody } from 'reactstrap';
   import pro from '../styles/img/pro.jpg'
+import '../styles/profile.css';
 
 
 class Profile extends Component {
   render() {
     return (
-    <>
-        <div className="topSpace">
+    <div className="topSpace">
+      <CardColumns className="cardStyle">
 
-          <Card className="block">
-            <div className="col-md-6">
-              <CardImg className="images" top width="100%" src={pro} alt="Card image cap" />
+        <Card >
+        <div className="warpCard">
+          <div className="colors">
+            <div className="row">
+              <div className=" user">
+                <CardImg top className="userImg"  src={pro} alt="Card image cap" />
+                <div className="userText">
+                  <CardSubtitle className="usernamePost">username</CardSubtitle>
+                  <CardText className="timePost">time.</CardText>
+                </div>
+              </div>
             </div>
-            <div className="col-md-6">  
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </div>  
 
-          </Card>
+            <div className="row">
+              <CardTitle className="recipeHead">Foodtext</CardTitle>
+            </div>
 
-          <Card className="block">
-            <CardImg className="images" top width="100%" src={pro} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+            <CardImg top width="100%" className="recipeImg" src={pro} alt="Card image cap" />
 
+            <div className="row">
+            <div className="cir">
+              ❤
+            </div>
+              <CardTitle className="recipeHead">Foodtext</CardTitle>
+            </div>
+          </div>  
+
+          {/* <div className="row">
+            <CardTitle className="ingred">Ingredients</CardTitle>
+          </div> */}
+          
+          <CardBody className="colorsTwo">
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <Button>Button</Button>
+          </CardBody>
         </div>
-    </>
+        </Card>
+
+        <Card >
+
+          <div className="row">
+            <div className="col-3">
+              <CardImg top height="62px"  src={pro} alt="Card image cap" />
+            </div>
+            <div className="col-9">
+              <CardSubtitle>username</CardSubtitle>
+              <CardText>time.</CardText>
+            </div>
+
+          </div>
+
+          <CardTitle>Card title</CardTitle>
+          <CardImg top width="100%" src={pro} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+
+        <Card >
+
+          <div className="row">
+            <div className="col-3">
+              <CardImg top height="62px"  src={pro} alt="Card image cap" />
+            </div>
+            <div className="col-9">
+              <CardSubtitle>username</CardSubtitle>
+              <CardText>time.</CardText>
+            </div>
+
+          </div>
+
+          <CardTitle>Card title</CardTitle>
+          <CardImg top width="100%" src={pro} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+
+        
+      
+      </CardColumns>
+    </div>
     )
   }
 }
